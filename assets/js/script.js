3e4b85ce6033f206ae8a1a96e6bbc43c3100eead
Carl Ambroselli <git@carl-ambroselli.de>
2017-08-16 22:53:06 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,10 +87,10 @@ function addScollListenerToId(id) {
 // ============================================================================
 // Try Buttons
 $('.tryButton').each(function() { 
-	$(this).attr('href', 'thanks.html?product=04_'+ window.location.host + '&'+window.location.search.replace('?', '')) 
+	$(this).attr('href', 'thanks.html?product=05_'+ window.location.host + '&'+window.location.search.replace('?', '')) 
 })
 $('.productButton').each(function() { 
-	$(this).attr('href', 'product.html?product=04_'+ window.location.host + '&'+window.location.search.replace('?', '')) 
+	$(this).attr('href', 'product.html?product=05_'+ window.location.host + '&'+window.location.search.replace('?', '')) 
 })
 
 $('.tryButton, .productButton').each(function() {
